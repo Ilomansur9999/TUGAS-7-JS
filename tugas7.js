3ab8953b59c4440d7a80712ceecea6294abf3787
ilomansur9999 <ilofaqot@gmail.com>
2020-05-03 15:15:30 +0700

    TUGAS 7 JS
    Tugas ini dikumpulkan ke NIOMIC

---
+++ tugas7.js
@@ -0,0 +1,26 @@
+var tinggiZ =[155,155,154,165,135,145,151,152,153,195]
+for(var i=0;i<tinggiZ.length;i++){
+	console.log(tinggiZ[i])
+}
+
+
+
+var tinggiX = [155,155,154,165,135,145,151,152,153,195];
+for (let x of tinggiX){
+	console.log(x);
+}
+
+
+
+
+// var tinggiZ =[155,155,154,165,135,145,151,152,153,195]
+// for(var i=0;i<tinggiZ.length;i++){
+// 	console.log("TinggiZ " + tinggiZ[i])
+// }
+
+
+
+// var tinggiX = [155,155,154,165,135,145,151,152,153,195];
+// for (let x of tinggiX){
+// 	console.log("TinggiX " + x);
+// }
